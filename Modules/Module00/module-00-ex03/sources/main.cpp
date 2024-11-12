fd9a8f10685aa863f131802dded7480e9e3c911e
afmyhouse <af.myhouse@gmail.com>
2024-11-12 17:52:07 +0000

    Creating Phonebook

---
+++ Modules/Module00/module-00-ex03/sources/main.cpp
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antoda-s <antoda-s@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/08 20:09:53 by antoda-s          #+#    #+#             */
+/*   Updated: 2024/04/19 12:19:27 by antoda-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "PhoneBook.hpp"
+#include <stdlib.h>
+
+void header(void)
+{
+	std::cout << " ██████╗ ██╗  ██╗ ██████╗ ███╗   ██╗███████╗██████╗  ██████╗  ██████╗ ██╗  ██╗\n"
+			  << " ██╔══██╗██║  ██║██╔═══██╗████╗  ██║██╔════╝██╔══██╗██╔═══██╗██╔═══██╗██║ ██╔╝ \n"
+			  << " ██████╔╝███████║██║   ██║██╔██╗ ██║█████╗  ██████╔╝██║   ██║██║   ██║█████╔╝ \n"
+			  << " ██╔═══╝ ██╔══██║██║   ██║██║╚██╗██║██╔══╝  ██╔══██╗██║   ██║██║   ██║██╔═██╗ \n"
+			  << " ██║     ██║  ██║╚██████╔╝██║ ╚████║███████╗██████╔╝╚██████╔╝╚██████╔╝██║  ██╗ \n"
+			  << " ╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝╚═════╝  ╚═════╝  ╚═════╝ ╚═╝  ╚═╝ \n"
+			  << std::endl;
+
+}
+
+int main(void)
+{
+	system("clear");
+	header();
+	PhoneBook	pb;
+	std::string	input;
+
+	pb = PhoneBook();
+	while (1)
+	{
+		pb._displayMenu();
+		std::getline(std::cin, input);
+		if (std::cin.eof())
+			break;
+		else
+		if (!input.compare("ADD"))
+		// if (input == "ADD")
+			pb.add();
+		else if (input == "SEARCH")
+			pb.search();
+		else if (input == "REMOVE")
+		{
+			std::string removeInput;
+			std::cout << "Enter index or phone number to remove: ";
+			std::getline(std::cin, removeInput);
+			if (isdigit(removeInput[0]))
+				pb.remove(std::stoi(removeInput));
+			else
+				pb.remove(removeInput);
+		}
+		else if (input == "BOOKMARK")
+		{
+			std::string bookmarkInput;
+			std::cout << "Enter index to bookmark: ";
+			std::getline(std::cin, bookmarkInput);
+			pb.bookmark(std::stoi(bookmarkInput));
+		}
+		else if (input == "EXIT")
+			break;
+		 else
+			pb._displayInvalid();
+	}
+}
