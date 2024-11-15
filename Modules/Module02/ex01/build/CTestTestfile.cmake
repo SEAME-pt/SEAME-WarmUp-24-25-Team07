cbd4e28786c80664cb63131f31d72a750600e49a
Anton Ferrus <af.myhouse@gmail.comcd>
2024-11-15 15:22:59 +0000

    Tests includes

---
+++ Modules/Module02/ex01/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/antoda-s/seame/Team07-SEAME-warm-up/Modules/Module02/ex01
+# Build directory: /home/antoda-s/seame/Team07-SEAME-warm-up/Modules/Module02/ex01/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Runs "/home/antoda-s/seame/Team07-SEAME-warm-up/Modules/Module02/ex01/build/parts")
+set_tests_properties(Runs PROPERTIES  _BACKTRACE_TRIPLES "/home/antoda-s/seame/Team07-SEAME-warm-up/Modules/Module02/ex01/CMakeLists.txt;12;add_test;/home/antoda-s/seame/Team07-SEAME-warm-up/Modules/Module02/ex01/CMakeLists.txt;0;")
